9b097b0e8addf18da6f6ef93bfe1c18a1ec7fefd
Loc Nguyen <loc.nguyen@vklink.vn>
2024-08-19 22:33:02 +0700

    init customer web

---
+++ nextjs/apps/customers/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-commerce website",
+  description: "It's an e-commerce website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
