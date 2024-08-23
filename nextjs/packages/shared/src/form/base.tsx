9db9d02283bc3e8b7cdef666d0de2173df6c6e70
Loc Nguyen <loc.nguyen@vklink.vn>
2024-08-23 11:45:44 +0700

    update workspace

---
+++ nextjs/packages/shared/src/form/base.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components";
+
+const BaseForm = () => {
+  return (
+    <div>
+      <p>Base Form</p>
+      <Button />
+    </div>
+  );
+};
+
+export default BaseForm;
